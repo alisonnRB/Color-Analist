7ae5bcdf60c6bc43dd91b2c7355b688dd1983e9f
alisonnRB <alisonrbjs@gmail.com>
2024-03-21 23:34:48 -0300

    api created

--- app/Http/Controllers/api/AnalistController.php
+++ app/Http/Controllers/api/AnalistController.php
@@ -4,18 +4,27 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use League\ColorExtractor\Client;
-use League\ColorExtractor\Image;
+
+use ColorThief\ColorThief;
 
 class AnalistController extends Controller
 {
     protected $image;
-    protected $paletteHex;
-    protected $paletteRgb;
+    protected $imageSize;
 
-    protected $mostPredominanceHEX;
-    protected $mostPredominanceRGB;
-    protected $representative;
+    protected $paletteHEX;
+    protected $paletteRGB;
+
+    protected $predominaceHEX;
+    protected $predominaceRGB;
+
+    protected $representativeHEX;
+
+    protected $representativeRBG;
+
+    protected $distintHex;
+    protected $distintRGB;
+
 
     public function index(Request $request)
     {
@@ -25,19 +34,27 @@ class AnalistController extends Controller
 
         if ($request->hasFile('image')) {
             $this->image = $this->Imageformatter($request->file('image'));
+            $this->imageSize = getimagesize($request->file('image'));
+
             if ($this->image) {
                 $this->paletteCreate();
-                $this->mostAndLessPredominance();
+                $this->PredominanceColor();
+                $this->RepresentativeColor();
+                $this->distintColor();
 
             } else {
                 return 'imagem de formato inválido';
             }
 
             $response = [
-                "palette-HEX" => $this->paletteHex,
-                "palette-RGB" => $this->paletteRgb,
-                "MostPredominanceHEX" => $this->mostPredominanceHEX,
-                "MostPredominanceRGB" => $this->mostPredominanceRGB,
+                "palette-HEX" => $this->paletteHEX,
+                "palette-RGB" => $this->paletteRGB,
+                "predomince-HEX" => $this->predominaceHEX,
+                "predomince-RGB" => $this->predominaceRGB,
+                "representative-HEX" => $this->representativeHEX,
+                "representative-RBB" => $this->representativeRBG,
+                "distinct-HEX" => $this->distintHex,
+                "distinct-RGB" => $this->distintRGB
             ];
 
             return $response;
@@ -61,32 +78,57 @@ class AnalistController extends Controller
         }
     }
 
+
     private function paletteCreate()
     {
-        $image = new Image($this->image);
-        $palette = $image->extract(5);
+        $image = new ColorThief;
+        $palette = $image->getPalette($this->image, 5, 5, null, 'hex');
 
-        $RgbList = [];
+        $RGBlist = [];
         foreach ($palette as $color) {
-            $color = $this->toRgb($color);
-            array_push($RgbList, $color);
+            array_push($RGBlist, $this->toRgb($color));
         }
 
-        $this->paletteHex = $palette;
-        $this->paletteRgb = $RgbList;
+        $this->paletteHEX = $palette;
+        $this->paletteRGB = $RGBlist;
+
     }
 
-    private function mostAndLessPredominance()
+    private function PredominanceColor()
     {
-        $image = new Image($this->image);
-        $colors = $image->extract(500);
-        $colorcount = array_count_values($colors);
+        $imagem = new ColorThief;
+        $color = $imagem->getPalette($this->image, 2, 10, null, 'hex');
 
-        $mostFrequent = array_search(max($colorcount), $colorcount);
-        $this->mostPredominanceHEX = $mostFrequent;
-        $this->mostPredominanceRGB = $this->toRgb($mostFrequent);
+        $this->predominaceHEX = $color[1];
+        $this->predominaceRGB = $this->toRgb($color[1]);
 
     }
+
+    private function distintColor()
+    {
+        $imagem = new ColorThief;
+        $color = $imagem->getPalette($this->image, 5, 10, null, 'hex');
+
+        $this->distintHex = $color[3];
+        $this->distintRGB = $this->toRgb($color[3]);
+
+    }
+
+    private function RepresentativeColor()
+    {
+        $colorThief = new ColorThief;
+        $whidth = intval(($this->imageSize[0] * 65) / 100);
+        $height = intval(($this->imageSize[1] * 65) / 100);
+
+        $w = intval(10);
+        $h = intval(10);
+
+        $representativeColor = $colorThief->getColor($this->image, 10, ['x' => $whidth, 'y' => $height, 'h' => $h, 'w' => $w], 'hex');
+
+        $this->representativeHEX = $representativeColor;
+        $this->representativeRBG = $this->toRgb($representativeColor);
+    }
+
 
     public function toRgb($hex)
     {
